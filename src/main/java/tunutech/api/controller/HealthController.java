51c997351aaf566d5a4f73f55c6215fe128abc62
Genhl <levihaoudou@gmail.com>
2025-11-21 21:03:01 +0000

    fix controller

--- src/main/java/tunutech/api/controller/HealthController.java
+++ src/main/java/tunutech/api/controller/HealthController.java
@@ -1,7 +1,11 @@
 package tunutech.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/test")
 public class HealthController {
     @GetMapping("/health")
     public String health() {
